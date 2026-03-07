3338feb4b62af98e41fa654054cf388f6d3e5a3f
vantu <tulevan526@gmail.com>
2026-03-07 10:09:38 +0700

    feat(chat-memory): configure MessageWindowChatMemory with JDBC persistence

--- section04/springai/src/main/java/com/vantu/springai/config/ChatMemoryChatClientConfig.java
+++ section04/springai/src/main/java/com/vantu/springai/config/ChatMemoryChatClientConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,11 @@ import java.util.List;
 
 @Configuration
 public class ChatMemoryChatClientConfig {
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10).chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
+
     // phải thêm tên để đảm bảo inject đúng bean vì có bean đã trả về cùng ChatClient rồi
     @Bean(name = "chatMemoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {

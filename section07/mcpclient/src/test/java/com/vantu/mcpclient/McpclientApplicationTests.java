d3b742896895d42bc268d45f32f26756b5475b78
vantu <tulevan526@gmail.com>
2026-03-16 16:51:22 +0700

    feat(section07): add mcp project in section 07

---
+++ section07/mcpclient/src/test/java/com/vantu/mcpclient/McpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantu.mcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
